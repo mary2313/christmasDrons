f9dd9b228d64eb3ace407eee9d579f8c32ae5204
mary2313 <71594228+mary2313@users.noreply.github.com>
2021-12-08 00:54:11 +0300

    Add files via upload

---
+++ christmasDrons-main/christmasDrons-main/DronCities/Assets/FindMinDistance.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DronCities.Assets
+{
+	public class FindMinDistance
+	{
+
+		public List<City> LeftDown = new List<City>();
+		public List<City> LeftUp = new List<City>();
+		public List<City> RightDown = new List<City>();
+		public List<City> RightUp = new List<City>();
+
+
+		public FindMinDistance(Country country)
+		{
+			for(int i = 0; i < country.Cities.Count; i++)
+			{
+				if((country.StartPoint.y - country.Cities[i].y) < 0 && (country.StartPoint.x - country.Cities[i].x) < 0)
+				{
+					RightUp.Add(country.Cities[i]);
+					country.Cities[i].color = new CityColor(50,70,130);
+				}
+				else if((country.StartPoint.y - country.Cities[i].y) < 0 && (country.StartPoint.x - country.Cities[i].x) > 0)
+				{
+					RightDown.Add(country.Cities[i]);
+					country.Cities[i].color = new CityColor(0, 255, 8);
+				}
+				else if((country.StartPoint.y - country.Cities[i].y) > 0 && (country.StartPoint.x - country.Cities[i].x) > 0)
+				{
+					LeftUp.Add(country.Cities[i]);
+					country.Cities[i].color = new CityColor(0, 0, 255);
+				}
+				else
+				{
+					LeftDown.Add(country.Cities[i]);
+					country.Cities[i].color = new CityColor(200, 0, 130);
+				}
+			}
+
+		}
+
+		public void FindAllDistanceFromStartPoint()
+		{
+
+		}
+
+	}
+}
---
+++ christmasDrons-main/christmasDrons-main/DronCities/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using Microsoft.VisualBasic.FileIO;
+using DronCities.Assets;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace DronCities
+{
+
+	public partial class Form1 : Form
+	{
+		
+		private OpenFileDialog ofd;
+
+		public Form1()
+		{
+			InitializeComponent();
+			//WindowState = FormWindowState.Maximized;
+			//TopMost = true;
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			
+			
+			ofd = new OpenFileDialog();
+			if (ofd.ShowDialog() == DialogResult.OK)
+			{
+				
+				var parser = new TextFieldParser(ofd.FileName,Encoding.Default);
+				var result = Parse.ParseCVSFile(parser);
+				string RES = "";
+				//result.Encoding
+				//byte[] bytes = Encoding.Default.GetBytes(result);
+				//result = Encoding.UTF8.GetString(bytes);
+				
+				int i = 0;
+				int count = 1;
+				string[] textResult = new string[5];
+				foreach(var item in result)
+				{
+					//textResult[i] += count +") "+ item + Environment.NewLine;
+					textResult[i] += item + ",";
+					i++;
+					if (i == 5)
+					{
+						i = 0;
+						count += 1;
+					}
+					GoProgressBar(1, 10000);
+
+				}
+				
+				richTextBox2.Text += textResult[0];
+				richTextBox3.Text += textResult[1];
+				richTextBox4.Text += textResult[2];
+				richTextBox5.Text += textResult[3];
+				richTextBox6.Text += textResult[4];
+
+				var Test = new Country(textResult[0], textResult[1], textResult[2], textResult[3], textResult[4]);
+
+				button1.Visible = false;
+				button2.Visible = true;
+
+				
+			}
+			
+		}
+
+		void GoProgressBar(int arg, int arg_lengt)
+		{
+			int a = (arg_lengt / 100) * arg;
+			Convert.ToInt32(a);
+			if(progressBar1.Value + a > 100) progressBar1.Value = 100 - a;
+			progressBar1.Value += a;
+		}
+
+		
+		private void button2_Click(object sender, EventArgs e)
+		{
+			HideAllStartForms();
+			pictureBox1.Visible = true;
+			button3.Visible = true;
+			Country Russia = new Country(richTextBox2.Text, richTextBox3.Text, richTextBox4.Text, richTextBox5.Text, richTextBox6.Text);
+			
+			Draw(Russia);
+
+			//Thread.Sleep(3000);
+			
+
+		}
+
+		private void Draw(Country country)
+		{
+			//Graphics graph = pictureBox1.CreateGraphics();
+			Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+			Graphics graph = Graphics.FromImage(bmp);
+			List<Rectangle> rect = new List<Rectangle>();
+            //Rectangle rect = ;
+            for (int i = 0; i < country.Cities.Count; i++)
+            {
+                rect.Add(new Rectangle(Convert.ToInt32(Math.Round(country.Cities[i].y * 10)) - 150, Convert.ToInt32(Math.Round(country.Cities[i].x * 10)) - 150, 2, 2));
+            }
+
+
+            graph.DrawRectangle(Pens.Red, new Rectangle(Convert.ToInt32(Math.Round(country.StartPoint.y * 10)) - 150, Convert.ToInt32(Math.Round(country.StartPoint.x * 10)) - 150, 2, 2));
+            graph.FillRectangle(
+                new SolidBrush(Color.FromArgb(255, country.StartPoint.color.R, country.StartPoint.color.G, country.StartPoint.color.B)),
+                new Rectangle(Convert.ToInt32(Math.Round(country.StartPoint.y * 10)) - 150, Convert.ToInt32(Math.Round(country.StartPoint.x * 10)) - 150, 2, 2)
+            );
+
+
+            FindMinDistance MinDistance = new FindMinDistance(country);
+
+			DrawSide(graph, MinDistance);
+			for (int i = 0; i < country.Cities.Count; i++)
+			{
+				try
+				{
+					graph.DrawRectangle(new Pen(Color.FromArgb(255, 192, 255, 255)), rect[i]);
+					graph.FillRectangle(new SolidBrush(Color.FromArgb(255, country.Cities[i].color.R, country.Cities[i].color.G, country.Cities[i].color.B)), rect[i]);
+
+				}
+				catch (NullReferenceException)
+				{
+					continue;
+				}
+			}
+
+			pictureBox1.Image = bmp;
+
+			
+
+			textBox2.Text = $"Слева : {MinDistance.LeftDown.Count} городов, справа : {MinDistance.RightDown.Count} городов!";
+			textBox2.Visible = true;
+			
+		}
+
+		private void DrawSide(Graphics graph, FindMinDistance MinDistance)
+		{
+			//int AllCities = MinDistance.LeftDown.Count + MinDistance.RightDown.Count + MinDistance.LeftUp.Count + MinDistance.RightUp.Count - 1;
+			List<Rectangle> rectLeftDown = new List<Rectangle>();
+			List<Rectangle> rectLeftUp = new List<Rectangle>();
+			List<Rectangle> rectRightDown = new List<Rectangle>();
+			List<Rectangle> rectRightUp = new List<Rectangle>();
+			///////////////////////////////////////////////////////
+			for (int i = 0; i < MinDistance.LeftDown.Count; i++)
+			{
+				rectLeftDown.Add(new Rectangle(Convert.ToInt32(Math.Round(MinDistance.LeftDown[i].y * 10)) - 150, Convert.ToInt32(Math.Round(MinDistance.LeftDown[i].x * 10)) - 50, 2, 2));
+			}
+			for (int i = 0; i < MinDistance.LeftUp.Count; i++)
+			{
+				rectLeftUp.Add(new Rectangle(Convert.ToInt32(Math.Round(MinDistance.LeftUp[i].y * 10)) - 150, Convert.ToInt32(Math.Round(MinDistance.LeftUp[i].x * 10)) - 50, 2, 2));
+			}
+			for (int i = 0; i < MinDistance.RightDown.Count; i++)
+			{
+				rectRightDown.Add(new Rectangle(Convert.ToInt32(Math.Round(MinDistance.RightDown[i].y * 10)) - 150, Convert.ToInt32(Math.Round(MinDistance.RightDown[i].x * 10)) - 50, 2, 2));
+			}
+			for (int i = 0; i < MinDistance.RightUp.Count; i++)
+			{
+				rectRightUp.Add(new Rectangle(Convert.ToInt32(Math.Round(MinDistance.RightUp[i].y * 10)) - 150, Convert.ToInt32(Math.Round(MinDistance.RightUp[i].x * 10)) - 50, 2, 2));
+			}
+			////////////////////////////////////////////////////////
+			
+			
+			
+		}
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
