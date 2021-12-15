84e1ec162fc6a270263705388c83661965fe2cf6
mary2313 <71594228+mary2313@users.noreply.github.com>
2021-12-15 20:59:45 +0300

    Add files via upload

---
+++ christmasDrons-main/christmasDrons-main/DronCities/Assets/Parse.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic.FileIO;
+
+namespace DronCities.Assets
+{
+	class Parse
+	{
+		static public List<string> ParseCVSFile(TextFieldParser parser)
+		{
+			List<string> result = new List<string>();
+			parser.TextFieldType = FieldType.Delimited;
+			parser.SetDelimiters(",");
+			int i = 0;
+			while (!parser.EndOfData)
+			{
+
+				string[] fields = parser.ReadFields();
+				foreach (string field in fields)
+				{
+					result.Add(field);
+					i++;
+				}
+			}
+			
+			return result;
+		}
+	}
+}
